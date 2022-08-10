175db26e570d6408f7b72139f65431823cfd4f06
moon-haha <0_pumps.untaxed@icloud.com>
2022-08-10 17:21:21 +0900

    rapid A + B

--- 03/07-15552.js
+++ 03/07-15552.js
@@ -4,6 +4,8 @@ const inputData = fs.readFileSync(filepath).toString().split("\n");
 
 //입력 첫 줄에 테스트케이스의 개수 T가 주어진다.
 let testcase = +inputData[0];
+//속도를 위해 한번에 출력하도록 한다.
+let answer = "";
 solution(testcase);
 function solution(T) {
   //출력 각 테스트케이스마다 A+B를 한 줄에 하나씩 순서대로 출력한다.
@@ -11,6 +13,7 @@ function solution(T) {
     let splitnumber = inputData[i].split(" ");
     let A = +splitnumber[0];
     let B = +splitnumber[1];
-    console.log(A + B);
+    answer += A + B + "\n";
   }
+  console.log(answer);
 }
